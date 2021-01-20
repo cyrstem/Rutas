c0f442378cac0ee09e50c37604d2b2158330647b
jacos <cyrstem@gmail.com>
2021-01-19 20:32:10 -0500

    gltf

--- pages/cojitambo.vue
+++ pages/cojitambo.vue
@@ -8,6 +8,7 @@
 
 import * as THREE from "three";
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 export default {
     layout:'controls',
@@ -19,68 +20,88 @@ export default {
             scene: null,
             renderer:null,
             gltf:null,
-             orbit_control:null
+            orbit_control:null,
         };
     },
-  mounted() {
-
-        let container = this.$refs.container;
-        this.camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,100)
-        this.camera.position.set(0,2,5)
-        this.scene = new THREE.Scene()
-
-   
-        // RENDER IN PAGE
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setClearColor("#000000");
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(this.renderer.domElement);
-    
-    ///Geometry
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    this.scene.add( cube );
-    
-///OBJ
-   
-const loader = new GLTFLoader();
-
-loader.load( 'cojitambo.gltf', function ( gltf ) {
-
-	this.scene.add( gltf.scene );
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} )
+    methods:{
+      init(){
+          let container = this.$refs.container;
+          this.camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,100)
+          this.camera.position.set(0,2,5)
+          this.scene = new THREE.Scene()
+          this.renderer = new THREE.WebGLRenderer({ antialias: true });
+          this.renderer.setClearColor("#000000");
+          this.renderer.setSize(window.innerWidth, window.innerHeight);
+          container.appendChild(this.renderer.domElement);
+      ///Geometry
+          const geometry = new THREE.BoxGeometry();
+          const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+          const cube = new THREE.Mesh( geometry, material );
+          this.scene.add( cube );
 
 
+       // SET ORBIT CONTROLS
+          this.orbit_controls = new OrbitControls(
+            this.camera,
+            this.renderer.domElement
+          )
+          this.orbit_controls.addEventListener("change", () =>
+            this.renderer.render(this.scene, this.camera)
+          )
+      },
+      loadModel() {
+        const loader = new GLTFLoader();
+        loader.load(
+          // resource URL
+          'model/cojitambo.gltf',
+          // called when the resource is loaded
+          function ( gltf ) {
 
-     // SET ORBIT CONTROLS
-    this.orbit_controls = new OrbitControls(
-      this.camera,
-      this.renderer.domElement
-    )
-    this.orbit_controls.addEventListener("change", () =>
-      this.renderer.render(this.scene, this.camera)
-    )
+            this.scene.add( gltf.scene );
 
-    window.addEventListener('resize',() =>
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene; // THREE.Group
+            gltf.scenes; // Array<THREE.Group>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+          },
+          // called while loading is progressing
+          function ( xhr ) {
+
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+          },
+          // called when loading has errors
+          function ( error ) {
+
+            console.log( 'An error happened'+ error );
+            
+
+          }
+        );
+    },
+      onResize(){
+        window.addEventListener('resize',() =>
         this.renderer.setSize(window.innerWidth,window.innerHeight),
         this.camera.aspect = window.innerWidth/window.innerHeight,
         this.camera.updateProjectionMatrix()
-        
-    )
+        )
+      },
+      doshow(){
+           this.renderer.render(this.scene,this.camera)
+          this.renderer.setPixelRatio(window.devicePixelRatio)
+      },
+    
+    },
+mounted() {
 
+this.init();
+this.onResize();
+this.doshow();
+this.loadModel();
 
-    this.renderer.render(this.scene,this.camera)
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-
-}
-
-
+ }
 }
 </script>
 
