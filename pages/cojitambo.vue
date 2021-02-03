412a421334d46c8978b5aafdd5249a3751dc6535
jacos <cyrstem@gmail.com>
2021-02-03 15:13:25 -0500

    fresh start/

--- pages/cojitambo.vue
+++ pages/cojitambo.vue
@@ -6,102 +6,41 @@
 </template>
 <script>
 
-import * as THREE from "three";
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
-import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+//import test from '@/test.js'
+
 export default {
     layout:'controls',
     name:'tester',
 
-    data(){
-        return{
-            camera: null,
-            scene: null,
-            renderer:null,
-            gltf:null,
-            orbit_control:null,
-        };
+    data() {
+      return {
+        // model:{},
+        // objects:[],
+        // activeMesh: {},
+        // isLoaded: false,
+        // autoRotate: true
+      };
+    },
+    mounted() {
+      // const { autoRotate } = this;
+      // this.scene =SceneInit({ rootEl: this.$refs.container, autoRotate});
+      // this.loadModel();
     },
     methods:{
-      init(){
-          let container = this.$refs.container;
-          this.camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,100)
-          this.camera.position.set(0,2,5)
-          this.scene = new THREE.Scene()
-          this.renderer = new THREE.WebGLRenderer({ antialias: true });
-          this.renderer.setClearColor("#000000");
-          this.renderer.setSize(window.innerWidth, window.innerHeight);
-          container.appendChild(this.renderer.domElement);
-      ///Geometry
-          const geometry = new THREE.BoxGeometry();
-          const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-          const cube = new THREE.Mesh( geometry, material );
-          this.scene.add( cube );
+      // loadModel(){
+      //   this.isLoaded = false;
+      //   this.scene.loadModel('model/scene1.gltf', (model)=>{
+      //     model.name = 'tester';
 
+      //   })
+      //   this.model = model
+      //   this.scene.add(model)
 
-       // SET ORBIT CONTROLS
-          this.orbit_controls = new OrbitControls(
-            this.camera,
-            this.renderer.domElement
-          )
-          this.orbit_controls.addEventListener("change", () =>
-            this.renderer.render(this.scene, this.camera)
-          )
-      },
-      loadModel() {
-        const loader = new GLTFLoader();
-        loader.load(
-          // resource URL
-          'model/cojitambo.gltf',
-          // called when the resource is loaded
-          function ( gltf ) {
-
-            this.scene.add( gltf.scene );
-
-            gltf.animations; // Array<THREE.AnimationClip>
-            gltf.scene; // THREE.Group
-            gltf.scenes; // Array<THREE.Group>
-            gltf.cameras; // Array<THREE.Camera>
-            gltf.asset; // Object
-
-          },
-          // called while loading is progressing
-          function ( xhr ) {
-
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-          },
-          // called when loading has errors
-          function ( error ) {
-
-            console.log( 'An error happened'+ error );
-            
-
-          }
-        );
-    },
-      onResize(){
-        window.addEventListener('resize',() =>
-        this.renderer.setSize(window.innerWidth,window.innerHeight),
-        this.camera.aspect = window.innerWidth/window.innerHeight,
-        this.camera.updateProjectionMatrix()
-        )
-      },
-      doshow(){
-           this.renderer.render(this.scene,this.camera)
-          this.renderer.setPixelRatio(window.devicePixelRatio)
-      },
-    
-    },
-mounted() {
-
-this.init();
-this.onResize();
-this.doshow();
-this.loadModel();
-
- }
+      //   setTimeout(() =>{
+      //     this.isLoaded = true
+      //   }, 200)
+      // }
+    }
 }
 </script>
 
