0dcec419856c84e5ff7414fa65c1abbfebaee125
jacos <cyrstem@gmail.com>
2021-02-05 18:31:45 -0500

    finally three

--- pages/cojitambo.vue
+++ pages/cojitambo.vue
@@ -22,20 +22,17 @@ export default {
  methods: {
     init() {
         const container = document.getElementById('container');
-        this.mesh = new Three.Object3D();
+        //this.mesh = new Three.Object3D();
 
         this.camera = new Three.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.25, 20);
-        this.camera.position.z = 1;
+        this.camera.position.z =10 ;
 
         this.scene = new Three.Scene();
-
-        // let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
-        // let material = new Three.MeshNormalMaterial();
-
-        // this.mesh2 = new Three.Mesh(geometry, material);
-        // this.scene.add(this.mesh2);
-     
-
+        this.light = new Three.DirectionalLight(0xffffff,1.8);
+        this.light.position.x =60;
+        this.light.position.y =100;
+        this.light.position.z =20
+        this.scene.add(this.light)
         const loader = new GLTFLoader().setPath('model/');
         loader.load('Cojitambo.gltf', (gltf)=>{
             this.mesh = gltf.scene
@@ -61,7 +58,7 @@ export default {
         this.animate();
     },
     render() {
-        requestAnimationFrame(this.render);
+        //requestAnimationFrame(this.render);
         // this.mesh.rotation.x += 0.01;
         // this.mesh.rotation.y += 0.02;
         
@@ -73,7 +70,7 @@ export default {
   mounted() {
       this.init();
       this.render();
-      window.addEventListener( 'resize', this.onWindowResize );
+      //window.addEventListener( 'resize', this.onWindowResize );
   },
  
        
